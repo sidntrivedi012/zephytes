c6b2bb8917cf3a7e2108c8f92b7c12e534c0c302
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-11-27 03:59:19 +0530

    added async/await

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,5 @@
 const Octokit = require("@octokit/rest");
-// const fs = require("fs");
+const fs = require("fs");
 
 //authentication
 require("dotenv").config();
@@ -11,38 +11,35 @@ const octokit = new Octokit({
 let obj = [];
 
 //function to fetch leaderboard
-function getLeaderboard() {
-  return new Promise((resolve, reject) => {
-    octokit.repos.listForOrg({ org: "osdc" }).then(repos => {
-      repos.data.forEach(({ name }) => {
-        octokit.repos
-          .listCommits({ owner: "osdc", repo: name })
-          .then(commits => {
-            commits.data.forEach(commit => {
-              commit_author = commit.commit.author.name;
-              temp = obj.find(e => e.contributor === commit_author);
-              if (temp != undefined) {
-                temp.commits += 1;
-              } else {
-                var contributor_data = {
-                  contributor: commit_author,
-                  commits: 1
-                };
-                obj.push(contributor_data);
-              }
-              obj.sort(function(a, b) {
-                return b.commits - a.commits;
-              });
-              resolve(obj);
+async function getLeaderboard() {
+  await octokit.repos.listForOrg({ org: "osdc" }).then(async repos => {
+    await repos.data.forEach(async ({ name }) => {
+      await octokit.repos
+        .listCommits({ owner: "osdc", repo: name })
+        .then(async commits => {
+          await commits.data.forEach(async commit => {
+            commit_author = commit.commit.author.name;
+            temp = obj.find(e => e.contributor === commit_author);
+            if (temp != undefined) {
+              temp.commits += 1;
+            } else {
+              var contributor_data = {
+                contributor: commit_author,
+                commits: 1
+              };
+              obj.push(contributor_data);
+            }
+            obj.sort(function(a, b) {
+              return b.commits - a.commits;
             });
           });
-      });
+          console.log(obj);
+          fs.writeFile("data.json", JSON.stringify(obj), function(err) {
+            if (err) throw err;
+          });
+        });
     });
   });
 }
-
+getLeaderboard();
 module.exports = { getLeaderboard };
-
-// fs.writeFile("final.json", JSON.stringify(obj), function(err) {
-//   if (err) throw err;
-// });
