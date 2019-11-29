00a8ca276d3d71700ff9470b1904ef452c9ddebe
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-11-29 12:25:41 +0530

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cron = require("node-cron");
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const lb = require("./leaderboard");
 app.set("view engine", "ejs");
 
